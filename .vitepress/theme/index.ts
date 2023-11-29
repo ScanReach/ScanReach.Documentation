b8eb3cde7e0ba960c620682d742ed1538ad46bae
Mathias Haugsbø <mathiash98@gmail.com>
2023-11-29 16:12:18 +0100

    :rocket: AI search with Documate

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,7 +3,8 @@ import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
-// import { Documate } from "@documate/vue";
+import { Documate } from "@documate/vue";
+import "@documate/vue/dist/style.css";
 
 export default {
   extends: DefaultTheme,
@@ -11,10 +12,10 @@ export default {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
       // TODO: Finish Documate integration for AI chatbot with OpenAI https://documate.site/integration/vitepress
-      // "nav-bar-content-before": () =>
-      //   h(Documate, {
-      //     endpoint: "",
-      //   }),
+      "nav-bar-content-before": () =>
+        h(Documate, {
+          endpoint: "https://z2adrrpr42.us.aircode.run/ask",
+        }),
     });
   },
   enhanceApp({ app, router, siteData }) {
